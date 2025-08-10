409de3c009c8f2cd25c10bd7c1aa03705eb7f281
clrhoades1 <clrhoades1@gmail.com>
2025-08-10 14:43:55 -0400

    converted data to be written to Parquet file

--- collector.py
+++ collector.py
@@ -1,34 +1,17 @@
 import requests
 import logging
-import mysql.connector
-from mysql.connector import Error
 from datetime import datetime, timedelta
 import time
 import os
+import pandas as pd
+from datetime import datetime
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Function to create a MySQL database and table for storing data
-def create_database():
-    try:
-        connection = mysql.connector.connect(
-            host=os.getenv('MYSQL_HOST'),
-            database=os.getenv('MYSQL_DATABASE'),
-            user=os.getenv('MYSQL_USER'),
-            password=os.getenv('MYSQL_PASSWORD')
-        )
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute('''CREATE TABLE IF NOT EXISTS device_data 
-                             (timestamp TEXT, device_id TEXT, current_temperature REAL, target_temperature REAL)''')
-            connection.commit()
-            cursor.close()
-    except Error as e:
-        logging.error(f"Error connecting to MySQL DB: {e}")
-    finally:
-        if connection.is_connected():
-            connection.close()
+def generate_parquet_file_name():
+    return "temperature-sensor-" + datetime.today().strftime('%Y-%m-%d') + ".parquet"
+
 
 # Function to login and retrieve the access token
 def login():
@@ -43,26 +26,23 @@ def login():
     token_info = response.json()
     return token_info['access_token'], datetime.now() + timedelta(seconds=token_info['expires_in'])
 
-# Function to insert data into the MySQL database
+# Function to insert data into Parquet File
 def insert_data(timestamp, device_id, current_temperature, target_temperature):
-    try:
-        connection = mysql.connector.connect(
-            host=os.getenv('MYSQL_HOST'),
-            database=os.getenv('MYSQL_DATABASE'),
-            user=os.getenv('MYSQL_USER'),
-            password=os.getenv('MYSQL_PASSWORD')
-        )
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute("INSERT INTO device_data VALUES (%s, %s, %s, %s)", 
-                           (timestamp, device_id, current_temperature, target_temperature))
-            connection.commit()
-            cursor.close()
-    except Error as e:
-        logging.error(f"Error connecting to MySQL DB: {e}")
-    finally:
-        if connection.is_connected():
-            connection.close()
+    parquet_file_name = os.getenv('PARQUET_FOLDER_PATH') + "" + generate_parquet_file_name()
+    
+    # Read existing Parquet file to DataFrame (if it exists)
+    parquet_df = pd.DataFrame()
+
+    if(os.path.exists(parquet_file_name)):
+        parquet_df = pd.read_parquet(parquet_file_name)
+    
+    # Append new value to it
+    parquet_df = pd.concat([parquet_df, pd.DataFrame({'timestamp': [timestamp], 'current temperature': [current_temperature], "target temperature": [target_temperature]})], ignore_index=True)
+    
+    # Write DataFrame back ot the file
+    parquet_df.to_parquet(parquet_file_name)
+
+    
 
 # Function to query the Homebridge API
 def query_homebridge_api(token):
@@ -72,6 +52,8 @@ def query_homebridge_api(token):
         response = requests.get(device_url, headers=headers)
         response.raise_for_status()
         data = response.json()
+
+        # Here is where we will query to get the values we want to extract. 
         
         current_temperature = data['values']['CurrentTemperature']
         target_temperature = data['values']['TargetTemperature']
@@ -83,7 +65,6 @@ def query_homebridge_api(token):
 
 def main():
     try:
-        create_database()
         token, token_expiry = login()
 
         while True:  
