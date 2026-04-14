ea096fb6dd96d356e5768bc2617e0726cbed44d3
Connor Rhoades <connorrhoades@Connors-MacBook-Air.local>
2026-04-14 18:11:00 -0400

    fixed ddevice name issue

--- collector.py
+++ collector.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.INFO)
 
 LOCK_FILE = "/tmp/collector.lock"
 LOG_LINE_PATTERN = re.compile(
-    r".*?\[(?P<timestamp>\d{1,2}/\d{1,2}/\d{4},\s*\d{1,2}:\d{2}:\d{2}\s*(?:AM|PM))\].*?\[(?P<device>[^\]]+)\]\s+current power\s+\[(?P<power>[^\]]+)\]\s+current voltage\s+\[(?P<voltage>[^\]]+)\]\s+current current\s+\[(?P<current>[^\]]+)\]"
+    r".*?\[(?P<timestamp>\d{1,2}/\d{1,2}/\d{4},\s*\d{1,2}:\d{2}:\d{2}\s*(?:AM|PM))\].*?\[eWeLink\]\[39m\s+\[(?P<device>[^\]]+)\]\s+current power\s+\[(?P<power>[^\]]+)\]\s+current voltage\s+\[(?P<voltage>[^\]]+)\]\s+current current\s+\[(?P<current>[^\]]+)\]"
 )
 
 
