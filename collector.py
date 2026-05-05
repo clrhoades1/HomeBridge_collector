6eb0df87e31f4bc9b19c2af160150d0bdcfc5634
Connor Rhoades <connorrhoades@Connors-MacBook-Air.local>
2026-05-05 18:09:23 -0400

    more

--- collector.py
+++ collector.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.INFO)
 
 LOCK_FILE = "/tmp/collector.lock"
 LOG_LINE_PATTERN = re.compile(
-    r".*?\[(?P<timestamp>\d{1,2}/\d{1,2}/\d{4},\s*\d{1,2}:\d{2}:\d{2}\s*(?:AM|PM))\].*?\[\s*eWeLink\s*\](?:\[\d+m\]?)?.*?\[(?P<device>[^\]]+)\].*?\[(?P<power>[^\]]+)\](?:.*?\[(?P<voltage>[^\]]+)\])?(?:.*?\[(?P<current>[^\]]+)\])?"
+    r".*?\[(?P<timestamp>\d{1,2}/\d{1,2}/\d{4},\s*\d{1,2}:\d{2}:\d{2}\s*(?:AM|PM))\].*?\[\s*eWeLink\s*\](?:\[\d+(?:;\d+)*m\]?)*.*?\[(?P<device>[^\]]+)\].*?\[(?P<power>[^\]]+)\](?:.*?\[(?P<voltage>[^\]]+)\])?(?:.*?\[(?P<current>[^\]]+)\])?"
 )
 
 
